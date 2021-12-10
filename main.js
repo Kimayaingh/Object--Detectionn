d7e415df8e211a4163c3f1c2dd7b43492be3c7d8
Kimayaingh <80696634+Kimayaingh@users.noreply.github.com>
2021-12-10 16:44:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+function bottle(){
+    window.location.replace("bottle.html");
+}
+
+function plant(){
+    window.location.replace("plant.html");
+}
+
+function chair(){
+    window.location.replace("chair.html");
+}
